c26f58ad83f1561c060347155f678c70e514c074
garthendrich <garthendrich@gmail.com>
2023-05-03 17:36:17 +0800

    feat: create render loop

--- src/main.js
+++ src/main.js
@@ -15,5 +15,21 @@ function main() {
     new Planet(3, [11, -15, -30]),
     new Planet(3, [-30, -22, -60]),
   ];
-  renderer.renderObjects(planets);
+
+  const SPEED = 0.8; // ! test/demo code
+  const FARTHEST_Z = 100; // ! test/demo code
+  let z = 0; // ! test/demo code
+  let incrementer = -SPEED; // ! test/demo code
+
+  window.requestAnimationFrame(loop);
+
+  function loop() {
+    if (z < 0 || z > FARTHEST_Z) incrementer *= -1; // ! test/demo code
+    renderer.moveCamera(0, 0, incrementer); // ! test/demo code
+    z += incrementer; // ! test/demo code
+
+    renderer.renderObjects(planets);
+
+    window.requestAnimationFrame(loop);
+  }
 }
