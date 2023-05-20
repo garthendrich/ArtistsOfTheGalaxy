07aa4d19d4b1df21ecb68210a3b1ac87d7325b26
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2023-05-20 08:08:25 +0800

    feat: updated vertices

--- src/entities/Collectibles.js
+++ src/entities/Collectibles.js
@@ -22,64 +22,87 @@ export default class Collectibles extends Entity {
 
   _generateVertices(length) {
     const vertices = [
-      0,
-      0,
-      0,
-      1,
-
-      length,
-      0,
-      0,
-      1,
-
-      0,
-      0,
-      length,
-      1,
-
-      length,
-      0,
-      length,
-      1,
-
-      0,
       -length,
-      0,
-      1,
-
-      length,
-      -length,
-      0,
-      1,
-
-      0,
       -length,
       length,
-      1,
-
       length,
       -length,
       length,
-      1,
+      length,
+      length,
+      length,
+      -length,
+      length,
+      length,
+
+      -length,
+      -length,
+      -length,
+      -length,
+      length,
+      -length,
+      length,
+      length,
+      -length,
+      length,
+      -length,
+      -length,
+
+      -length,
+      length,
+      -length,
+      -length,
+      length,
+      length,
+      length,
+      length,
+      length,
+      length,
+      length,
+      -length,
+
+      -length,
+      -length,
+      -length,
+      length,
+      -length,
+      -length,
+      length,
+      -length,
+      length,
+      -length,
+      -length,
+      length,
+
+      length,
+      -length,
+      -length,
+      length,
+      length,
+      -length,
+      length,
+      length,
+      length,
+      length,
+      -length,
+      length,
+
+      -length,
+      -length,
+      -length,
+      -length,
+      -length,
+      length,
+      -length,
+      length,
+      length,
+      -length,
+      length,
+      -length,
     ];
     const indices = [
-      // TOP FACE TRIANGLES
-      1, 2, 3, 2, 3, 4,
-
-      // BOTTOM FACE TRIANGLES
-      5, 6, 7, 6, 7, 8,
-
-      // LEFT FACE TRIANGLES
-      5, 7, 1, 7, 1, 3,
-
-      // RIGHT FACE TRIANGLES
-      2, 6, 8, 8, 2, 4,
-
-      // FRONT FACE TRIANGLES
-      1, 5, 6, 6, 1, 2,
-
-      // BACK FACE TRIANGLES
-      3, 7, 8, 8, 3, 4,
+      0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12,
+      14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
     ];
 
     return [indices, vertices];
