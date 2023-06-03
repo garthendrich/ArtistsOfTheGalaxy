3d80e2faad696ab5126f38661d01e39364dfc337
jcsestilo <jsestilo@up.edu.ph>
2023-06-03 17:27:31 +0800

    add: add collision detection for ship, collectible

--- src/events/objectCollision.js
+++ src/events/objectCollision.js
@@ -16,7 +16,44 @@ export function hasCollided(type, object1, object2){
         console.log(distance < object1.getRadius() + object2.getRadius());
         return distance < object1.getRadius() + object2.getRadius();
     } else if(type==="entityToShip"){
+        const shipWidth = 0;
+        const shipHeight = 0;
+        const shipDepth = 0;
 
+        const shipBoundingBox = {
+            minX: object1.getX() - shipWidth / 2,
+            maxX: object1.getX() + shipWidth / 2,
+            minY: object1.getY() - shipHeight / 2,
+            maxY: object1.getY() + shipHeight / 2,
+            minZ: object1.getZ() - shipDepth / 2,
+            maxZ: object1.getZ() + shipDepth / 2
+        };
+
+        const objectWidth = 0;
+        const objectHeight = 0;
+        const objectDepth = 0;
+
+        const objectBoundingBox = {
+            minX: object2.getX() - objectWidth / 2,
+            maxX: object2.getX() + objectWidth / 2,
+            minY: object2.getY() - objectHeight / 2,
+            maxY: object2.getY() + objectHeight / 2,
+            minZ: object2.getZ() - objectDepth / 2,
+            maxZ: object2.getZ() + objectDepth / 2
+        };
+
+        if (
+            shipBoundingBox.minX <= objectBoundingBox.maxX &&
+            shipBoundingBox.maxX >= objectBoundingBox.minX &&
+            shipBoundingBox.minY <= objectBoundingBox.maxY &&
+            shipBoundingBox.maxY >= objectBoundingBox.minY &&
+            shipBoundingBox.minZ <= objectBoundingBox.maxZ &&
+            shipBoundingBox.maxZ >= objectBoundingBox.minZ
+        ) {
+            // Collision detected
+            console.log("Collectible collected!");
+            return true;
+        }
     }
 
 
